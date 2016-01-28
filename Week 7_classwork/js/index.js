a4e24c88ff7e10c83f8403512680a6d30fc3db7e
Darien82 <DarienCurry@gmail.com>
2016-01-27 20:11:58 -0800

    making comments for week 7

--- Week 7_classwork/js/index.js
+++ Week 7_classwork/js/index.js
@@ -8,10 +8,11 @@ $(document).ready(function()
           var inputVal1 = $("#valToConvertToF");
             var outputVal1 = $("#result1");    
         // read value entered by user
+        
         // convert it from string to number
         var initialVal1 = parseFloat(inputVal1.val());
         console.log(initialVal1);
-        // bulletproof
+        
         if(isNaN(initialVal1))
         {
             window.alert("Enter a valid number!");
